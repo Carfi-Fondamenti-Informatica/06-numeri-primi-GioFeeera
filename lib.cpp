c67f5a40870a417b0e480c6a4de412f7b41c24d9
GioFeeera <117982966+GioFeeera@users.noreply.github.com>
2022-12-03 09:55:22 +0000

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
+int numprimo(int m, int n){
+    bool x=true;
+    if (m=!1){
+        if ((n % m) !=0) {
+            m--;
+            x = numprimo(m,n);
+        }
+    else {
+        x= false;
+    }
+    }
+    else {
+        x = true;
+    }
 
+    return x;
+}
