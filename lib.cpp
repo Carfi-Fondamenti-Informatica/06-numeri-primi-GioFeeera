9ebf5ae33896d3ca3eb7afaff503e9faef99d1c6
GioFeeera <117982966+GioFeeera@users.noreply.github.com>
2022-12-03 10:08:41 +0000

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,14 +1,14 @@
 #include "lib.h"
-int numprimo(int m, int n){
+int primo (int m, int n){
     bool x=true;
     if (m=!1){
         if ((n % m) !=0) {
             m--;
-            x = numprimo(m,n);
+            x = primo(m,n);
         }
-    else {
-        x= false;
-    }
+        else {
+            x= false;
+        }
     }
     else {
         x = true;
