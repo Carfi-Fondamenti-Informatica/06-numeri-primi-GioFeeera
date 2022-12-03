e63c875b1d8839a713cf3bca85dd9586c1171644
GioFeeera <117982966+GioFeeera@users.noreply.github.com>
2022-12-03 09:57:08 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+    int n=0;
+    cout <<"inserisci un numero "; cin >> n;
+    if (numprimo (m,n)==true){
+        cout << "numero primo"<<endl;
+    }
+    else if (numprimo(m,n)==false){
+        cout << "numero non primo" << endl;
+    }
+
+    return 0;
 }
