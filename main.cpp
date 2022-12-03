c2eb3caff7de29d14eacbf18bfa548f21cc794c4
GioFeeera <117982966+GioFeeera@users.noreply.github.com>
2022-12-03 10:06:53 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
     int n=0;
+    int m= (n-1);
     cout <<"inserisci un numero "; cin >> n;
     if (numprimo (m,n)==true){
         cout << "numero primo"<<endl;
