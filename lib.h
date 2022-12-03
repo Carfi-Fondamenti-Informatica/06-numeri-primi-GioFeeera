86ba38625566937d6d09d1590da82c3cfc85ce01
GioFeeera <117982966+GioFeeera@users.noreply.github.com>
2022-12-03 10:09:10 +0000

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
-#ifndef ESERCITAZIONE6_LIB_H
-#define ESERCITAZIONE6_LIB_H
-int numprimo(int, int);
-#endif //ESERCITAZIONE6_LIB_H
+#ifndef ESERCITAZIONE_6_LIB_H
+#define ESERCITAZIONE_6_LIB_H
+int primo (int, int);
+#endif //ESERCITAZIONE_6_LIB_H
